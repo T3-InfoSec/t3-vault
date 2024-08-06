61010f2d68b0867a6c27c777194ce4a638dedb1a
flatFeather <github.astronaut143@silomails.com>
2024-08-06 02:19:46 +0200

    Add knowledge types activity test

---
+++ greatwall_protocol_flutter/test/knowledge_types_activity_test.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:greatwall_protocol_flutter/knowledge_types_activity.dart';
+
+void main() {
+  testWidgets('KnowledgeTypesActivity has the correct AppBar and buttons', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: KnowledgeTypesActivity(),
+      ),
+    );
+
+    expect(find.text('Greatwall TKBA Protocol'), findsOneWidget);
+
+    // Verify that there are four buttons, each with the text from knowledgeTypes.
+    final List<String> knowledgeTypes = ["Hashviz", "Formosa", "Fractal", "Voice"];
+    for (String type in knowledgeTypes) {
+      expect(find.text(type), findsOneWidget);
+    }
+
+    final appBar = tester.widget<AppBar>(find.byType(AppBar));
+    expect(appBar.backgroundColor, const Color(0xFF70A8FF));
+
+    final buttonFinder = find.byType(ElevatedButton);
+    for (final button in buttonFinder.evaluate()) {
+      final elevatedButton = button.widget as ElevatedButton;
+      final buttonColor = elevatedButton.style?.backgroundColor?.resolve({}) ?? Colors.transparent;
+
+      expect(buttonColor, const Color(0xFF70A8FF));
+    }
+  });
+}
