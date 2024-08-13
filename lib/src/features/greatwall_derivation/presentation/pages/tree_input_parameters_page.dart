17daf7fabc0cb12637773eb668543fa77e6c9c53
flatFeather <github.astronaut143@silomails.com>
2024-08-13 13:07:10 +0200

    Update Tree input parameters page

--- lib/src/features/greatwall_derivation/presentation/pages/tree_input_parameters_page.dart
+++ lib/src/features/greatwall_derivation/presentation/pages/tree_input_parameters_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'package:great_wall/great_wall.dart';
+
 import 'package:t3_vault/src/features/greatwall_derivation/presentation/widgets/custom_elevated_button_widget.dart';
+import 'package:t3_vault/src/features/greatwall_derivation/presentation/widgets/custom_text_field_widget.dart';
 import 'package:t3_vault/src/features/greatwall_derivation/presentation/widgets/password_text_field_widget.dart';
 import 'package:t3_vault/src/features/greatwall_derivation/presentation/widgets/theme_dropdown_button_widget.dart';
 
@@ -9,6 +12,7 @@ import 'package:t3_vault/src/features/greatwall_derivation/presentation/widgets/
 /// The [TreeInputsParametersPage] class includes a dropdown to select a theme and several buttons
 /// to configure TLP, tree depth, and tree arity. It also includes a password field and a
 /// final "Derive" button.
+
 class TreeInputsParametersPage extends StatefulWidget {
   const TreeInputsParametersPage({super.key});
 
@@ -19,8 +23,12 @@ class TreeInputsParametersPage extends StatefulWidget {
 
 class TreeInputsParametersPageState extends State<TreeInputsParametersPage> {
   final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _tlpController = TextEditingController();
+  final TextEditingController _depthController = TextEditingController();
+  final TextEditingController _arityController = TextEditingController();
+
   String? _selectedTheme;
-  // Temporarily hardcoded list of themes
+
   final List<String> _themes = ["BIP39", "medieval fantasy", "sci-fi"];
 
   @override
@@ -46,30 +54,34 @@ class TreeInputsParametersPageState extends State<TreeInputsParametersPage> {
               },
             ),
             const SizedBox(height: 20),
-            CustomElevatedButton(
-                text: 'Choose TLP',
-                onPressed: () {
-                  // TODO: Implement functionality for this button
-                }),
+            CustomTextField(
+              hintText: 'Choose TLP',
+              controller: _tlpController,
+            ),
             const SizedBox(height: 20),
-            CustomElevatedButton(
-                text: 'Choose tree depth',
-                onPressed: () {
-                  // TODO: Implement functionality for this button
-                }),
+            CustomTextField(
+              hintText: 'Choose tree depth',
+              controller: _depthController,
+            ),
             const SizedBox(height: 20),
-            CustomElevatedButton(
-                text: 'Choose tree arity',
-                onPressed: () {
-                  // TODO: Implement functionality for this button
-                }),
+            CustomTextField(
+              hintText: 'Choose tree arity',
+              controller: _arityController,
+            ),
             const SizedBox(height: 30),
             PasswordTextField(controller: _passwordController),
             const SizedBox(height: 80),
             CustomElevatedButton(
                 text: 'Derive',
                 onPressed: () {
-                  // TODO: Implement functionality for this button
+                  GreatWall greatWall = GreatWall(
+                      timeLockPuzzleParam:
+                          int.tryParse(_tlpController.text) ?? 0,
+                      treeDepth: int.tryParse(_depthController.text) ?? 0,
+                      treeArity: int.tryParse(_arityController.text) ?? 0);
+
+                  greatWall.seed0 = _passwordController.text;
+                  // TODO: Go to derivation process with greatwall object.
                 }),
           ],
         ),
