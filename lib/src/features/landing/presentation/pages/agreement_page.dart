4ba11cc7c639ce06b4baa49fb8cf4e9ab06ae22c
MuhammadMuradG <muhammad.murad.eg@gmail.com>
2024-08-24 17:31:58 +0300

    Organize imports.

--- lib/src/features/landing/presentation/pages/agreement_page.dart
+++ lib/src/features/landing/presentation/pages/agreement_page.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:go_router/go_router.dart';
 
 import '../bloc/bloc.dart';
-import 'policy_page.dart';
 
 /// Displays a list of SampleItems.
 class AgreementPage extends StatelessWidget {
