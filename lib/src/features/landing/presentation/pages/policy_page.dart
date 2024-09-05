0625b1e367fb08e93e3612bcc1326d96b0ef8515
Yuri S Villas Boas <yurisvb@protonmail.ch>
2024-09-05 17:16:13 +0200

    Changed gibberish for Lorem Ypsum in text  policy_page.dart

--- lib/src/features/landing/presentation/pages/policy_page.dart
+++ lib/src/features/landing/presentation/pages/policy_page.dart
@@ -42,9 +42,12 @@ class PolicyPage extends StatelessWidget {
           padding: EdgeInsets.all(10),
           child: Text(
             // TODO: Add policy and terms agreement
-            'dsfdsjfdsljfldsjflkds dsjfldskjflds fdskjflds fdsjfl dsfsdfsdfsf'
-            ' dslfalfjdskljflds lfdsljfldsfsd fsdjfl sdfjasl dfjlsf sdfsdfdsd'
-            ' sdfdsfdsfds dskjfldsjf sdlfjdsljfl sdslkjfsdl dslfjsdl dsfsddf.',
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do '
+            'eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, '
+            'quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. '
+            'Duis aute irure dolor in reprehenderit in voluptate velit esse '
+            'cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat '
+            'cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
           ),
         ),
       ),
