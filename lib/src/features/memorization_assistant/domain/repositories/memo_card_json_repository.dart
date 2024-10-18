a9cf3393c152de54a25f88329de41e18247c0d09
flatFeather <github.astronaut143@silomails.com>
2024-10-18 02:06:33 +0200

    Change memo card repository to manage memoCardIdMap

--- lib/src/features/memorization_assistant/domain/repositories/memo_card_json_repository.dart
+++ lib/src/features/memorization_assistant/domain/repositories/memo_card_json_repository.dart
@@ -6,8 +6,20 @@ import '../converters/memo_card_json_converter.dart';
 
 class MemoCardRepository {
   final String filePath;
+  final Map<String, MemoCard> _memoCardIdMap = {};
 
-  MemoCardRepository({required this.filePath});
+  MemoCardRepository({required this.filePath}) {
+    _loadMemoCardsFromFile();
+  }
+
+  /// Load memo cards from json file
+  /// 
+  /// This method initialises the map of ids and memocards
+  /// with the persisted values using the internal logic 
+  /// of the [readMemoCards] method.
+  Future<void> _loadMemoCardsFromFile() async {
+    await readMemoCards();
+  }
 
   /// Reads memo cards from a JSON file and returns a map of memo cards.
   ///
@@ -18,48 +30,50 @@ class MemoCardRepository {
   /// Each JSON object is converted to a [MemoCard] instance using 
   /// [MemoCardConverter.fromJson], and a corresponding ID is extracted from 
   /// the JSON object to serve as the key in the resulting map.
-  Future<Map<String, MemoCard>> readMemoCards() async {
+  Future<List> readMemoCards() async {
     try {
       final file = File(filePath);
       if (!await file.exists()) {
-        return <String, MemoCard>{};
+        return [];
       }
       final contents = await file.readAsString();
       final jsonData = jsonDecode(contents) as List;
 
-      final memoCardMap = <String, MemoCard>{};
       for (var json in jsonData) {
         final memoCard = MemoCardConverter.fromJson(json);
         final id = json['id'];
-        memoCardMap[id] = memoCard;
+        _memoCardIdMap[id] = memoCard;
       }
-      return memoCardMap;
+      return _memoCardIdMap.values.toList();
     } catch (e) {
       // TODO: Handle file read errors
-      return <String, MemoCard>{};
+      return [];
     }
   }
 
   /// Writes memo cards to a JSON file.
   ///
-  /// This method takes a map of memo cards, [memoCardMap], and writes their 
-  /// details to a JSON file located at [filePath]. Each entry in the map is 
-  /// converted to JSON format using [MemoCardConverter.toJson], and an 
-  /// additional 'id' field is included for each memo card, corresponding to 
-  /// its key in the map.
+  /// This method takes a list of memo cards, [memoCards], and writes their 
+  /// details to a JSON file located at [filePath]. Each memo card in the 
+  /// list is converted to JSON format using [MemoCardConverter.toJson], 
+  /// and a unique 'id' field is generated for each memo card.
   ///
   /// The resulting list of JSON objects is then encoded as a string and 
   /// written to the specified file. If the file does not exist, it will be 
   /// created.
-  Future<void> writeMemoCards(Map<String, MemoCard> memoCardMap) async {
+  Future<void> writeMemoCards(List<MemoCard> memoCards) async {
     final file = File(filePath);
 
-    final jsonData = memoCardMap.entries
-        .map((entry) => {
-              ...MemoCardConverter.toJson(entry.value),
-              'id': entry.key,
-            })
-        .toList();
+    _memoCardIdMap.clear();
+
+    final jsonData = memoCards.map((memoCard) {
+      final id = MemoCardConverter.generateId();
+      _memoCardIdMap[id] = memoCard;
+      return {
+        ...MemoCardConverter.toJson(memoCard),
+        'id': id,
+      };
+    }).toList();
 
     await file.writeAsString(jsonEncode(jsonData));
   }
