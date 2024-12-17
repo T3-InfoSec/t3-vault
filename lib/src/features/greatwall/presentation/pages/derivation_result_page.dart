b9b2a05676e2294a002bdf036d9fb79c63f5bdf4
flatFeather <github.astronaut143@silomails.com>
2024-12-17 08:38:07 +0100

    Add explanations

--- lib/src/features/greatwall/presentation/pages/derivation_result_page.dart
+++ lib/src/features/greatwall/presentation/pages/derivation_result_page.dart
@@ -13,6 +13,7 @@ import 'package:t3_vault/src/common/cryptography/presentation/widgets/password_p
 
 import 'package:t3_vault/src/features/greatwall/presentation/widgets/deckname_promt_widget.dart';
 import 'package:t3_vault/src/common/cryptography/presentation/widgets/eka_promt_widget.dart';
+import 'package:t3_vault/src/features/greatwall/presentation/widgets/info_widget.dart';
 import 'package:t3_vault/src/features/greatwall/states/derivation_state.dart';
 import 'package:uuid/uuid.dart';
 import '../../../../common/settings/presentation/pages/settings_page.dart';
@@ -54,8 +55,8 @@ class DerivationResultPage extends StatelessWidget {
                     children: [
                       Expanded(
                         child: TextField(
-                          controller: TextEditingController(
-                              text: ka.encodedHash),
+                          controller:
+                              TextEditingController(text: ka.encodedHash),
                           readOnly: true,
                           obscureText: !state.isKAVisible,
                           decoration: InputDecoration(
@@ -87,8 +88,7 @@ class DerivationResultPage extends StatelessWidget {
                   ElevatedButton(
                     onPressed: () async {
                       // Copy the seed to the clipboard for a limited time
-                      Clipboard.setData(ClipboardData(
-                          text: ka.formosa.seed));
+                      Clipboard.setData(ClipboardData(text: ka.formosa.seed));
                       ScaffoldMessenger.of(context).showSnackBar(
                         SnackBar(
                             content: Text(AppLocalizations.of(context)!
@@ -107,87 +107,120 @@ class DerivationResultPage extends StatelessWidget {
                   const SizedBox(height: 10),
                   BlocBuilder<MemoCardSetBloc, MemoCardSetState>(
                     builder: (context, memoCardSetState) {
-                      return ElevatedButton(
-                        onPressed: (memoCardSetState is MemoCardSetAddSuccess)
-                            ? null
-                            : () async {
-                                Eka eka = Eka();
-                                final String? optionalEka = await showDialog<String>(
-                                  context: context,
-                                  builder: (context) =>
-                                      EKAPromptWidget(eka: eka.key),
-                                );
-                                if (!context.mounted) return;
-                                if (optionalEka != null) {
-                                  final deckName = await showDialog<String>(
-                                    context: context,
-                                    builder: (context) =>
-                                        const DecknamePromtWidget(),
-                                  );
-                                  if (!context.mounted) return;
-                                  if (deckName != null) {
-                                    String? enteredEKA = await showDialog<String>(
+                      return Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          ElevatedButton(
+                            onPressed: (memoCardSetState
+                                    is MemoCardSetAddSuccess)
+                                ? null
+                                : () async {
+                                    Eka eka = Eka();
+                                    final String? optionalEka =
+                                        await showDialog<String>(
                                       context: context,
-                                      builder: (context) => const PasswordPrompt(),
+                                      builder: (context) =>
+                                          EKAPromptWidget(eka: eka.key),
                                     );
                                     if (!context.mounted) return;
-                                    if (enteredEKA != eka.key){
-                                      await showDialog<String>(
+                                    if (optionalEka != null) {
+                                      final deckName = await showDialog<String>(
                                         context: context,
-                                        builder: (context) => const InputKeyErrorPromtWidget(),
+                                        builder: (context) =>
+                                            const DecknamePromtWidget(),
                                       );
-                                    } else {
-                                      context.read<MemoCardSetBloc>().add(MemoCardSetCardsAdding());
-                                      final deckId = const Uuid().v4();
-                                      final deck = Deck(deckId, deckName);
-
-                                      final pa0Seed = Provider.of<DerivationState>(
-                                          context,
-                                          listen: false).pa0Seed;
-                                      final pa0 = Pa0(seed: pa0Seed);
-                                      await eka.encryptPa0(pa0);
-
-                                      List<MemoCard> memoCards = [];
-
-                                      memoCards.addAll([
-                                        EkaMemoCard(eka: 'Can you remember where you saved eka\'s backup?', deck: deck),
-                                        Sa0MemoCard(
-                                            sa0: base64Encode(pa0.seedEncrypted),
-                                            deck: deck)
-                                      ]);
                                       if (!context.mounted) return;
-                                      final tacitKnowledge = Provider.of<DerivationState>(
-                                          context,
-                                          listen: false)
-                                      .tacitKnowledge;
-                                      
-                                      for (int i = 1; i <= state.treeDepth; i++) {
-                                        final node = Node(state.savedNodes[i - 1]);
-                                        await eka.encryptNode(node);   
-                                        final selectedNode = Node(state.savedNodes[i]);
-                                        await eka.encryptNode(selectedNode);
-                                        memoCards.add(TacitKnowledgeMemoCard(
-                                            knowledge: {
-                                              'level': i,
-                                              'node': base64Encode(node.hashEncrypted),
-                                              'selectedNode': base64Encode(selectedNode.hashEncrypted),
-                                              'treeArity': state.treeArity,
-                                              'treeDepth': state.treeDepth,
-                                              'tacitKnowledge': tacitKnowledge,
-                                            },
-                                            deck: deck,
-                                            title: 'Derivation Level $i Card'));
+                                      if (deckName != null) {
+                                        String? enteredEKA =
+                                            await showDialog<String>(
+                                          context: context,
+                                          builder: (context) =>
+                                              const PasswordPrompt(),
+                                        );
+                                        if (!context.mounted) return;
+                                        if (enteredEKA != eka.key) {
+                                          await showDialog<String>(
+                                            context: context,
+                                            builder: (context) =>
+                                                const InputKeyErrorPromtWidget(),
+                                          );
+                                        } else {
+                                          context
+                                              .read<MemoCardSetBloc>()
+                                              .add(MemoCardSetCardsAdding());
+                                          final deckId = const Uuid().v4();
+                                          final deck = Deck(deckId, deckName);
+
+                                          final pa0Seed =
+                                              Provider.of<DerivationState>(
+                                                      context,
+                                                      listen: false)
+                                                  .pa0Seed;
+                                          final pa0 = Pa0(seed: pa0Seed);
+                                          await eka.encryptPa0(pa0);
+
+                                          List<MemoCard> memoCards = [];
+
+                                          memoCards.addAll([
+                                            EkaMemoCard(
+                                                eka:
+                                                    'Can you remember where you saved eka\'s backup?',
+                                                deck: deck),
+                                            Sa0MemoCard(
+                                                encryptedSa0: base64Encode(
+                                                    pa0.seedEncrypted),
+                                                deck: deck)
+                                          ]);
+                                          if (!context.mounted) return;
+                                          final tacitKnowledge =
+                                              Provider.of<DerivationState>(
+                                                      context,
+                                                      listen: false)
+                                                  .tacitKnowledge;
+
+                                          for (int i = 1;
+                                              i <= state.treeDepth;
+                                              i++) {
+                                            final node =
+                                                Node(state.savedNodes[i - 1]);
+                                            await eka.encryptNode(node);
+                                            final selectedNode =
+                                                Node(state.savedNodes[i]);
+                                            await eka.encryptNode(selectedNode);
+                                            memoCards.add(TacitKnowledgeMemoCard(
+                                                knowledge: {
+                                                  'level': i,
+                                                  'node': base64Encode(
+                                                      node.hashEncrypted),
+                                                  'selectedNode': base64Encode(
+                                                      selectedNode
+                                                          .hashEncrypted),
+                                                  'treeArity': state.treeArity,
+                                                  'treeDepth': state.treeDepth,
+                                                  'tacitKnowledge':
+                                                      tacitKnowledge,
+                                                },
+                                                deck: deck,
+                                                title:
+                                                    'Derivation Level $i Card'));
+                                          }
+                                          if (!context.mounted) return;
+                                          context.read<MemoCardSetBloc>().add(
+                                              MemoCardSetCardsAdded(
+                                                  memoCards: memoCards));
+                                        }
                                       }
-                                      if (!context.mounted) return;
-                                      context.read<MemoCardSetBloc>().add(
-                                          MemoCardSetCardsAdded(
-                                              memoCards: memoCards));
                                     }
-                                  }
-                                }
-                              },
-                        child: Text(
-                            AppLocalizations.of(context)!.saveDerivationCards),
+                                  },
+                            child: Text(AppLocalizations.of(context)!
+                                .saveDerivationCards),
+                          ),
+                          const SizedBox(width: 8),
+                          const InfoButton(
+                              tooltipText:
+                                  "An assistant will guide your memorization exercises and securely back up this information locally until it is fully memorized."
+                                  "With these exercises, you will effortlessly be able to input both explicit (seed) and tacit (choosing steps) information in a matter of days."),
+                        ],
                       );
                     },
                   ),
@@ -195,23 +228,28 @@ class DerivationResultPage extends StatelessWidget {
                   BlocBuilder<MemoCardSetBloc, MemoCardSetState>(
                     builder: (context, memoCardSetState) {
                       return ElevatedButton(
-                        onPressed: (memoCardSetState is MemoCardSetAdding) 
-                        ? () {
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            SnackBar(
-                              content: Text(AppLocalizations.of(context)!.savingInProgress),
-                              duration: const Duration(seconds: 10),
-                            ),
-                          );
-                        }
-                        : () {
-                          context.read<MemoCardSetBloc>().add(MemoCardSetUnchanged());
-                          context.read<GreatWallBloc>().add(GreatWallReset());
-                          context.pop();
-                        },
+                        onPressed: (memoCardSetState is MemoCardSetAdding)
+                            ? () {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(
+                                    content: Text(AppLocalizations.of(context)!
+                                        .savingInProgress),
+                                    duration: const Duration(seconds: 10),
+                                  ),
+                                );
+                              }
+                            : () {
+                                context
+                                    .read<MemoCardSetBloc>()
+                                    .add(MemoCardSetUnchanged());
+                                context
+                                    .read<GreatWallBloc>()
+                                    .add(GreatWallReset());
+                                context.pop();
+                              },
                         child: (memoCardSetState is MemoCardSetAdding)
-                          ? const CircularProgressIndicator()
-                          : Text(AppLocalizations.of(context)!.reset),
+                            ? const CircularProgressIndicator()
+                            : Text(AppLocalizations.of(context)!.reset),
                       );
                     },
                   ),
